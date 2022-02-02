647cf500e95d258e6fd0e5c3639dcaf7e22e584a
HusJAW <hj25691@gmail.com>
2022-02-02 18:19:41 +0000

    testing shape

---
+++ src/Shapeone.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Shapeone = () => {
+  return (
+    <>
+      <section className="w-2/4">
+        <div className=" shadow-[2px_3px_0px_black] max-w[300px] h-[213px] bg-white border-y-4 border-r-4 border-black relative grid place-items-center p-4 after:content-['']  after:absolute after:h-[180px] after:w-[39.36px] after:rounded-tr-[259.23px] after:shadow-[inset_-4px_1px_0px_black] after:border-black after:border-t-4 after:bottom-[-4px] after:left-[-39.36px] before:content-['']  before:absolute before:top-[-4px] before:left-[-39.36px] before:h-[35px] before:w-[50px] before:border-t-4 before:border-l-[4px] before:border-black"></div>
+      </section>
+    </>
+  );
+};
+
+export default Shapeone;
