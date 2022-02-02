499efc7165da36f9c32eb0279d9341107d6808af
HusJAW <hj25691@gmail.com>
2022-02-02 22:10:12 +0000

    shadow fill

--- src/Shapeone.js
+++ src/Shapeone.js
@@ -4,7 +4,9 @@ const Shapeone = () => {
   return (
     <>
       <section className="w-2/4">
-        <div className=" shadow-[2px_3px_0px_black] max-w[300px] h-[213px] bg-white border-y-4 border-r-4 border-black relative grid place-items-center p-4 after:content-['']  after:absolute after:h-[180px] after:w-[39.36px] after:rounded-tr-[259.23px] after:shadow-[inset_-4px_1px_0px_black] after:border-black after:border-t-4 after:bottom-[-4px] after:left-[-39.36px] before:content-['']  before:absolute before:top-[-4px] before:left-[-39.36px] before:h-[35px] before:w-[50px] before:border-t-4 before:border-l-[4px] before:border-black">
+        <div className=" shadow-[4px_6px_0px_black] max-w[300px] h-[213px] bg-red-500 border-y-4 border-r-4 border-black relative grid place-items-center p-4 after:content-['']  after:absolute after:h-[180px] after:w-[39.36px] after:rounded-tr-[259.23px] after:shadow-[inset_-4px_1px_0px_black] after:border-black after:border-t-4 after:bottom-[-4px] after:left-[-39.36px] before:content-['']  before:absolute before:top-[-4px] before:left-[-42.36px] before:h-[35px] before:w-[50px] before:border-t-4 before:border-l-[4px] before:border-black 
+        before:bg-red-500">
+            <span className="border-t-4 absolute h-[180px] w-[39.36px] rounded-tr-[259.23px] shadow-[8px_-20px_0px_red] border-black bottom-[-4px] left-[-39.36px] shadow-red-500"></span>
           <p>react please react</p>
         </div>
       </section>
